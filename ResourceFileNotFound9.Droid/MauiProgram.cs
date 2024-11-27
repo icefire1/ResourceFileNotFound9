6c16bc99c13f174df91d4d5105c4d2d449333245
Simon Larsen <simon333_333@hotmail.com>
2024-11-27 16:24:05 +0100

    Add project files.

---
+++ ResourceFileNotFound9.Droid/MauiProgram.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System.Globalization;
+
+namespace ResourceFileNotFound9.Droid
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+
+            builder
+                .UseSharedMauiApp();
+
+            return builder.Build();
+        }
+    }
+}
---
+++ ResourceFileNotFound9/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace ResourceFileNotFound9
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
